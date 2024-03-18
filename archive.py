2af585becac56583cb022e85d21231eba61e26ed
theBigUPs <114905556+theBigUPs@users.noreply.github.com>
2024-03-18 23:26:13 +0300

    added try catch for dropped connections

--- archive.py
+++ archive.py
@@ -1,7 +1,17 @@
 import os
+import time
 import yt_dlp
 import subprocess
+import requests
 #https://github.com/yt-dlp/FFmpeg-Builds?tab=readme-ov-file#ffmpeg-static-auto-builds for ffmpeg
+
+def check_internet_connection():
+    try:
+        response = requests.get("https://google.com", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False    
+
 
 def download_video(video_url, output_path="downloads", renamed_filename="downloaded_video"):
     # Download options for yt-dlp
@@ -14,15 +24,21 @@ def download_video(video_url, output_path="downloads", renamed_filename="downloa
         #"listformats":True
     }
 
-    # Download the video using yt-dlp
-    with yt_dlp.YoutubeDL(ydl_options) as ydl:
-        info = ydl.extract_info(video_url, download=True)
-
-    # Get the downloaded video file path
-
-    video_file_path = ydl.prepare_filename(info)
-    #print(video_file_path)# outputsdownloads/downloaded_video.mp4
-    return video_file_path, renamed_filename
+    while True:
+        try:
+            # Download the video using yt-dlp
+            with yt_dlp.YoutubeDL(ydl_options) as ydl:
+                info = ydl.extract_info(video_url, download=True)
+            # Get the downloaded video file path
+            video_file_path = ydl.prepare_filename(info)
+            return video_file_path, renamed_filename
+        except yt_dlp.utils.DownloadError:
+            print("Checking internet connection.")
+            if check_internet_connection():
+                print("something unexpected happened")
+            else:
+                print("No internet. Retrying in 20 seconds.")
+                time.sleep(20)  # Wait for 20 minutes before retrying
 
 
 def encode_video_nvenc(input_path, output_path, output_format='mp4', crf=23, max_framerate=30, resolution='854x480',bitrate="1000k"):
@@ -52,21 +68,21 @@ def encode_video_nvenc(input_path, output_path, output_format='mp4', crf=23, max
 
 
 if __name__ == "__main__":
-    url = f"""https://www.youtube.com/watch?v=PpoeqjyWdBs"""
+    url = f"""url here"""
 
     # Download the video
-    filename=r"FF VII Rebirth 6"
+    filename=r"""trying to chill"""
     video_path, video_title = download_video(url,renamed_filename=filename,output_path=f"F:\\downloads")
 
-    #video_path = f"""F:\\downloads\\res3.mp4"""
-    #video_title = f"""res3"""
+    #video_path = f"""F:\\downloads\\FF VII Rebirth (Gongaga) 7.mp4"""
+    #video_title = f"""FF VII Rebirth (Gongaga) 7"""
     # Define the output path for the re encoded video
     # {video_title}
     output = f"downloads/{video_title}_480p.mp4"
 
     # Encode the video using ffmpeg with NVENC, CRF, max framerate, and resolution
-    #854 x 480 1280 x 720
-    encode_video_nvenc(video_path, output, crf=35, max_framerate=30, resolution='854 x 480',bitrate="1500k")
+    #854x480 1280x720
+    encode_video_nvenc(video_path, output, crf=35, max_framerate=30, resolution='854x480',bitrate="1500k")
 
     print(f"finished")
 
